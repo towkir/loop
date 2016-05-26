d5a8d92bea6925541f31733f21cbe3e90d29eee5
Mark Banner <standard8@mozilla.com>
2016-05-26 14:00:27 +0100

    Try get functional tests running

---
+++ docker-functional/test.sh
@@ -0,0 +1,34 @@
+    #!/bin/bash -e
+    # WARNING: this is experimental mileage may vary!
+
+    # Image appears to be a Docker Hub Image. Fetch docker image
+    image_name=quay.io/mrrrgn/ubuntu-ci:0.0.1
+    image_name=loop-functional
+    #docker pull 'quay.io/mrrrgn/ubuntu-ci:0.0.1'
+
+    # Find a unique container name
+    container_name='task-HId0IKpkSt-w-bpAGwBMeQ-container'
+
+    # Run docker command
+    docker run -ti \
+      --name "${container_name}" \
+      -e GITHUB_EVENT='push' \
+      -e GITHUB_BRANCH='taskcluster-experimental' \
+      -e GITHUB_BASE_BRANCH='taskcluster-experimental' \
+      -e GITHUB_HEAD_REPO_URL='https://github.com/mozilla/loop.git' \
+      -e GITHUB_HEAD_USER='Standard8' \
+      -e GITHUB_HEAD_SHA='2e4aab4b38738aa3f0ccb64dc6bbeca3bff0d384' \
+      -e GITHUB_HEAD_BRANCH='taskcluster-experimental' \
+      -e GITHUB_HEAD_USER_EMAIL='Standard8@noreply.github.com' \
+      -e RUN_LOCALLY='true' \
+      -p 5900 \
+      ${image_name} \
+      /bin/bash -l -c './bin/setup.sh && git clone https://github.com/mozilla/loop.git repo && cd repo && git checkout taskcluster-experimental && mozdownload --type=daily --branch=mozilla-central && tar xfj *.bz2 && TEST_BROWSER=`pwd`/firefox/firefox TEST_SERVER=dev make functional'
+
+
+# git clone https://github.com/mozilla/loop.git repo && cd repo && git checkout taskcluster-experimental && mozdownload --type=daily --branch=mozilla-central && tar xfj *.bz2
+# TEST_BROWSER=`pwd`/firefox/firefox TEST_SERVER=dev make functional"
+#mozdownload --type=daily --branch=mozilla-central && tar xfj *.bz2
+
+    # Delete docker container
+    docker rm -v "${container_name}"
